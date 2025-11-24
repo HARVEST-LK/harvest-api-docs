096906e961b25be050a239d7742221f2cd9e6a55
Erandi Laksika <erandilakshika478@gmail.com>
2025-11-24 19:10:10 +0530

    Add sidebar and nojekyll config

---
+++ sidebars.js
@@ -0,0 +1,36 @@
+const sidebars = {
+  docs: [
+    {
+      type: "category",
+      label: "Overview",
+      items: ["api/overview"],
+    },
+    {
+      type: "category",
+      label: "GN Division Endpoints",
+      items: ["api/GN_DivisionCRUD"],
+    },
+    {
+      type: "category",
+      label: "AG Office Endpoints",
+      items: ["api/AG_OfficeCRUD"],
+    },
+    {
+      type: "category",
+      label: "Admin Endpoints",
+      items: ["api/AdminCRUD"],
+    },
+    {
+      type: "category",
+      label: "Farmer Endpoints",
+      items: ["api/FarmerCRUD"],
+    },
+    {
+      type: "category",
+      label: "File Upload",
+      items: ["api/fileUpload"],
+    },
+  ],
+};
+
+export default sidebars;
